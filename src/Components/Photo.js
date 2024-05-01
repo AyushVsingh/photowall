63279a282d928d88d9388bdbe5012887ded2693d
unknown <aayushsingh050801@gmail.com>
2024-05-01 13:58:49 +0530

    adding state to component instance

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Photo(props) {
-    const post = this.props.post
+    const post = props.post
     return <figure className="figure">
         <img className="photo" src={post.imageLink} alt={post.description} />
         <figcaption><p>{post.description}</p></figcaption>
