35b60ba95111c478bf7ef6dcba635367820f163e
unknown <aayushsingh050801@gmail.com>
2024-05-02 14:12:39 +0530

    adding lifecycle methods

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -22,13 +22,22 @@ class Main extends Component {
                 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
             }]
         }
-        this.removePhoto = this.removePhoto.bind(this);
+        this.removePhoto = this.removePhoto.bind(this)
+
     }
     removePhoto(postRemoved) {
         console.log(postRemoved.description)
         this.setState((state) => ({
             posts: state.posts.filter(post => post !== postRemoved)
         }))
+    }
+
+    componentDidMount() {
+
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevState.posts)
+        console.log(this.state)
     }
     render() {
         return <div>
@@ -37,5 +46,6 @@ class Main extends Component {
         </div>
     }
 }
+
 
 export default Main
